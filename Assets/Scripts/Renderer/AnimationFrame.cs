13f7200216104b422141e725a1517a4ed4d49b5f
Alexandre Brault <alex@3doubloons.net>
2012-10-06 00:41:55 +0000

    Variables de base de AnimationFrame

--- Assets/Scripts/Renderer/AnimationFrame.cs
+++ Assets/Scripts/Renderer/AnimationFrame.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
 using System.Collections;
 
-public class AnimationFrame {
+public class AnimationFrame
+{
+	public int X { get; private set; }
 
+	public int Y { get; private set; }
+
+	public int Width { get; private set; }
+
+	public int Height { get; private set; }
+
+	public int OffsetX { get; private set; }
+
+	public int OffsetY { get; private set; }
+	
+	public AnimationFrame (int x, int y, int width, int height, int offX = 0, int offY = 0)
+	{
+		X = x;
+		Y = y;
+		Width = width;
+		Height = height;
+		OffsetX = offX;
+		OffsetY = offY;
+	}
 }
