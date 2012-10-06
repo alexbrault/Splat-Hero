692b0e6a2aee7340cf05f10db14491540973a5b6
Alexandre Brault <alex@3doubloons.net>
2012-10-06 00:58:24 +0000

    SpriteAnimation a une liste de AnimationFrame

--- Assets/Scripts/Renderer/SpriteAnimation.cs
+++ Assets/Scripts/Renderer/SpriteAnimation.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
 
-public class Animation {
-
+public class SpriteAnimation : IEnumerable {
+	private List<AnimationFrame> frames = new List<AnimationFrame>();
+	
+	public void Add(AnimationFrame frame) {
+		frames.Add(frame);
+	}
+	
+	IEnumerator IEnumerable.GetEnumerator() {
+		return frames.GetEnumerator();
+	}
 }
