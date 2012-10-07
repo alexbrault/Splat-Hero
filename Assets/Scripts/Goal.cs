4f547a66facecb274dca049d0a7e8572f5b66154
Alexandre Brault <alex@3doubloons.net>
2012-10-07 05:02:53 +0000

    Ajusté le temps de splat

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -22,7 +22,7 @@ public class Goal : MonoBehaviour {
 	}
 	
 	private IEnumerator MakeBallSoonish() {
-			yield return new WaitForSeconds(2.5f);
+			yield return new WaitForSeconds(1.5f);
 			GameManager.Instance.NewBall();
 	}
 }
