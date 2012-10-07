ab7f6af18326cae6cccfe329c129e1d833424cdd
Alexandre Brault <alex@3doubloons.net>
2012-10-07 08:26:44 +0000

    Moins lette

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -10,8 +10,6 @@ public class Goal : MonoBehaviour {
 	public static Goal Player2_Goal;
 	
 	private void Start() {
-		label = GetComponentInChildren<GUIText>();
-		label.text = Score.ToString();
 		if (Owner == Player.PlayerID.PLAYER1 && Player1_Goal == null) {
 			Player1_Goal = this;
 			DontDestroyOnLoad(this);
@@ -27,7 +25,6 @@ public class Goal : MonoBehaviour {
 		GoblinBall candidate = other.gameObject.GetComponent<GoblinBall>();
 		if (candidate != null && candidate.isLocked) {
 			Score++;
-			label.text = Score.ToString();
 			GameManager.Instance.DestroyObject(candidate.gameObject);
 			StartCoroutine(MakeBallSoonish());
 			SplatterShower.Instance.ShowSplatter();
